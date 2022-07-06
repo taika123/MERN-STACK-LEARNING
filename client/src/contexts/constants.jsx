37aa2d7d71214f93e03bc168273104dc9f449e79
Doro <taika949@gmail.com>
2022-07-06 10:17:42 +0700

    final heroku

--- client/src/contexts/constants.jsx
+++ client/src/contexts/constants.jsx
@@ -1,9 +1,9 @@
 export const apiUrl =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : // : "https://fathomless-temple-42878.herokuapp.com/api";
+    : // : " https://hidden-woodland-48518.herokuapp.com//api";
       //   "someURL"
-      "https://secret-badlands-92990.herokuapp.com/api";
+      " https://hidden-woodland-48518.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "mern-learn";
 
